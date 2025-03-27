e3d671bcdd0cf78db0842dacd643a852bbc323fa
Zachary Miles <zsmiles@mail.fhsu.edu>
2025-03-26 21:28:12 -0400

    Categories change

--- api/categories/read_single.php
+++ api/categories/read_single.php
@@ -1,34 +1,26 @@
 <?php
-    //Headers
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
- 
-    include_once '../../config/Database.php';
-    include_once '../../models/Category.php';
- 
- 
-    //Instantiate DB and CONNECT
-    $database = new Database();
-    $db = $database->connect();
- 
- 
-    //Instantiate blog category object
-    $cat = new Category($db);
- 
- 
-    //GET ID
-    $cat->id = isset($_GET['id']) ? $_GET['id'] : die();
- 
-    //GET category
-   if( $cat->read_single()){
-        echo json_encode(array(
-            'id' => $cat->id,
-            'category' => $cat->category
-        ));
-   }
-   //cannot find id
-   else {
-    echo json_encode(array(
-        'message' => 'category_id Not Found'
-    ));
-   }
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$cat = new Category($db);
+$cat->id = isset($_GET['id']) ? $_GET['id'] : null;
+
+if ($cat->id === null) {
+    echo json_encode(["message" => "Missing Required Parameters"]);
+    exit();
+}
+
+if ($cat->read_single()) {
+    echo json_encode([
+        "id" => $cat->id,
+        "category" => $cat->category
+    ]);
+} else {
+    echo json_encode(["message" => "category_id Not Found"]);
+}
+?>
