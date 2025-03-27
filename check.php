ce4270e24e1181b767ee5928c7a4df9c6542bb82
Zachary Miles <zsmiles@mail.fhsu.edu>
2025-03-26 22:44:19 -0400

    Change

--- check.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo 'USERNAME: ' . getenv('USERNAME') . "<br>";
-echo 'PASSWORD: ' . getenv('PASSWORD') . "<br>";
-echo 'DBNAME: ' . getenv('DBNAME') . "<br>";
-echo 'HOST: ' . getenv('HOST') . "<br>";
-echo 'PORT: ' . getenv('PORT') . "<br>";
-?>
