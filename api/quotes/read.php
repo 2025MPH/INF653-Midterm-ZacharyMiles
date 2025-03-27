63102c66d9bedb394a0a5b5482b01745944e4299
Zachary Miles <zsmiles@mail.fhsu.edu>
2025-03-26 20:46:05 -0400

    More change

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -13,35 +13,36 @@
     //Instantiate quote object
     $quotes = new Quote($db);
 
-    //Assign filter parameters if set
-    if (isset($_GET['author_id'])){
+    //If filters are set, assign them
+    if (isset($_GET['author_id'])) {
         $quotes->author_id = $_GET['author_id'];
     }
-    if (isset($_GET['category_id'])){
+    if (isset($_GET['category_id'])) {
         $quotes->category_id = $_GET['category_id'];
     }
 
-    //Execute the query
+    //Execute query
     $result = $quotes->read();
     $num = $result->rowCount();
 
     //Initialize array to hold quotes
     $quotes_arr = array();
 
-    if($num > 0){
-        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+    if ($num > 0) {
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            // Each row should have id, quote, author, and category
             extract($row);
             $quote_item = array(
                 'id'       => $id,
-                'author'   => $author,
                 'quote'    => html_entity_decode($quote),
+                'author'   => $author,
                 'category' => $category
             );
-            array_push($quotes_arr, $quote_item);
+            $quotes_arr[] = $quote_item;
         }
         echo json_encode($quotes_arr);
     } else {
-        // Return an empty array if no quotes are found
+        // Return an empty array instead of a message object
         echo json_encode([]);
     }
 ?>
